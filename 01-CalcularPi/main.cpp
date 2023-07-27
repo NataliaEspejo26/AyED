ac2a256d7d598e778cbc740327e907da28c9f028
NataliaEspejo26 <nespejo@frba.utn.edu.ar>
2023-07-27 17:48:46 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -4,20 +4,18 @@ using namespace std;
 
 int main() {
     double pi = 0.0;
-    int den = 1;
     int signo = 1;
+    
+    for ( int i = 1; ; i+=2)
+ {
+    pi += signo * (4.0 / i);
+    signo=-signo;
 
-    int i = 0;
-    int maxI = 1000000;
-
-    while ( i < maxI ) {
-        pi += signo * (4.0 / den);
-        den += 2;
-        signo = -signo;
-        i++;
-    }
-
+    if ( pi < 3.141593 && i > 2000000)
+    {
+        break;
+    } 
+ }   
     cout << "El valor aproximado de pi es: " << fixed << setprecision(6)<< pi << endl;
-
     return 0;
 }

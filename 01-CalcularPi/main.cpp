6fa3b5df9d28789844568f0367c6f8b68024f2ec
NataliaEspejo26 <nespejo@frba.utn.edu.ar>
2023-07-28 10:24:51 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     double pi = 0.0;
     int signo = 1;
-    int vueltas;
+    
     for ( int i = 1; ; i+=2)
  {
     pi += signo * (4.0 / i);
     signo=-signo;
-    vueltas++;
-    if ( pi < 3.141593 && vueltas > 2000000)
+    
+    if ( pi > 3.1415915 && pi < 3.141593 )
     {
         break;
     } 

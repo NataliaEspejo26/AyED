64356ce9a3d625fad211245c89be5c4d79e74847
NataliaEspejo26 <nespejo@frba.utn.edu.ar>
2023-07-29 06:55:55 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -3,19 +3,28 @@
 using namespace std;
 
 int main() {
-    double pi = 0.0;
-    int signo = 1;
-    
-    for ( int i = 1; ; i+=2)
- {
-    pi += signo * (4.0 / i);
-    signo=-signo;
-    
-    if ( pi > 3.1415915 && pi < 3.141593 )
-    {
-        break;
-    } 
- }   
-    cout << "El valor aproximado de pi es: " << fixed << setprecision(6)<< pi << endl;
-    return 0;
+	double pi = 0.0;
+
+	// coloco cualquier valor que sea lo suficientemente 
+	// distante de pi para que la condicion del while se cumpla
+	double prev_pi = -1.0;
+
+	int signo = 1;
+	int i = 1;
+
+	// mientras la diferencia entre pi y el valor previo de pi 
+	// no sea lo suficientemente chica para tener la presicion 
+	// mencionada en el ejercicio, continuará ciclando
+	while (abs(pi - prev_pi) > 0.000001) { 
+		prev_pi = pi;
+		pi += signo * (4.0 / i);
+		signo = -signo;
+		i += 2;
+	}
+
+	// al salir del ciclo whille, obtuvimos una diferencia 
+	// entre pi y su valor previo de pi lo suficientemente
+	// chica para obtener el resultado esperado
+	cout << "El valor aproximado de pi es: " << fixed << setprecision(6) << pi << endl;
+	return 0;
 }
